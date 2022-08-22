ae08760b5dda1be69f836f65beb7672c6cc82cdb
woogi <dnrlwo11@gmail.com>
2022-08-22 22:54:13 +0900

    [FIX] delete unused file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -18,13 +17,3 @@ const NotFoundPage = ({ data, location }) => {
 export const Head = () => <Seo title="404: Not Found" />
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
