9589fc948ab41d45b4ca69a32ecec76e901e8924
woogi <dnrlwo11@gmail.com>
2022-08-22 23:15:17 +0900

    [FIX] gql되살림

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,15 +7,34 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Seo = ({ description, lang, title, children }) => {
+  const { site } = useStaticQuery(
+    graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                    description
+                    social {
+                        twitter
+                    }
+                }
+            }
+        }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
 
   return (
     <>
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
       <meta
@@ -23,19 +42,16 @@ const Seo = ({ description, lang, title, children }) => {
         content={site.siteMetadata?.social?.twitter || ``}
       />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
   )
 }
-
 Seo.defaultProps = {
   description: ``,
 }
-
 Seo.propTypes = {
   description: PropTypes.string,
   title: PropTypes.string.isRequired,
 }
-
 export default Seo
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,10 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-
   return (
     <Layout location={location} title={siteTitle}>
       <h1>404: Not Found</h1>
@@ -17,3 +16,13 @@ const NotFoundPage = ({ data, location }) => {
 export const Head = () => <Seo title="404: Not Found" />
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
