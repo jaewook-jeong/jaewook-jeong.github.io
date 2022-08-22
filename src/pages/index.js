c08b3918459164b89727e35948cbffab4943b4d6
woogi <dnrlwo11@gmail.com>
2022-08-22 21:20:14 +0900

    [CHORE] git user change

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,7 @@ const BlogIndex = ({ data, location }) => {
       <Layout location={location} title={siteTitle}>
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+         git test
         </p>
       </Layout>
     )
