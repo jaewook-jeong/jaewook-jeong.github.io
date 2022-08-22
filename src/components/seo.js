a85a3b347705abffddc00f766d1433896c0b2698
woogi <dnrlwo11@gmail.com>
2022-08-22 22:56:25 +0900

    [FIX] delete unused file

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,34 +7,15 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 const Seo = ({ description, lang, title, children }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            social {
-              twitter
-            }
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
+      <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
       <meta
@@ -42,7 +23,7 @@ const Seo = ({ description, lang, title, children }) => {
         content={site.siteMetadata?.social?.twitter || ``}
       />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:description" content={description} />
       {children}
     </>
   )
