db17096e659eac9acb6e2c807dbe29db3df6eab5
woogi <dnrlwo11@gmail.com>
2022-09-10 23:05:46 +0900

    [FEAT] bio관련 수정

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,6 +8,29 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components";
+
+const StyledBio = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  div {
+    display: flex;
+  }
+  
+  .author-socials {
+    gap: 10px;
+    color: #1a202c;
+  }
+  
+  a {
+    
+    :hover {
+      color: #0077aa;
+    }
+  }
+`;
+
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -30,24 +53,28 @@ const Bio = () => {
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
-  console.log(social);
+
   return (
-    <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="https://avatars.githubusercontent.com/u/56660101?v=4"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      <p>
-        Written by <strong>{author.name}</strong>
-        <br/>
-        문의사항이 있다면 언제든 연락주세요:)
-      </p>
+    <StyledBio>
+      <div>
+        <StaticImage
+          className="bio-avatar"
+          layout="fixed"
+          formats={["auto", "webp", "avif"]}
+          src="https://avatars.githubusercontent.com/u/56660101?v=4"
+          width={75}
+          height={75}
+          quality={95}
+          alt="Profile picture"
+        />
+        <p>
+          Written by <strong>{author.name}</strong>
+          <br/>
+          문의사항이 있다면 언제든 연락주세요:)
+          <br/>
+          <a href="https://www.nextunicorn.kr">@nextunicorn</a>
+        </p>
+      </div>
       <div className="author-socials">
         {social.github && (
           <a href={`https://github.com/${social.github}`}>GitHub</a>
@@ -56,7 +83,7 @@ const Bio = () => {
           <a href={`https://velog.io/${social.velog}`}>Velog</a>
         )}
       </div>
-    </div>
+    </StyledBio>
   )
 }
 
