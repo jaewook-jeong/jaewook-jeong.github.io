eed8367a477779755c45093cf94a4c97aa8dab8f
woogi <dnrlwo11@gmail.com>
2022-09-16 22:53:15 +0900

    [CHORE] seo title

--- src/pages/index.js
+++ src/pages/index.js
@@ -89,7 +89,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="All posts" />
+export const Head = () => <Seo title="Home" />
 
 export const pageQuery = graphql`
   query {
