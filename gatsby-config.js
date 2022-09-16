2c1db93affb6640b3f922321c068746f0946ffc4
woogi <dnrlwo11@gmail.com>
2022-09-16 22:52:46 +0900

    [CHORE] add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,12 +112,12 @@ module.exports = {
         name: `Woogi Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#fefefe`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.ico`,
       },
     },
     {
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,13 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <GlobalCss />
-      <header className="global-header">{header}</header>
+      <header className="global-header">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        {header}
+      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
