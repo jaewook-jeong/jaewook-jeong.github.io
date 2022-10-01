506df2ca777eb021b4b9c0b13cef2256b9737f4d
woogi <dnrlwo11@gmail.com>
2022-10-01 10:44:52 +0900

    [FEAT] 404페이지 이미지 추가

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import Image from 'gatsby-image'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Bio from "../components/bio";
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   return (
     <Layout location={location} title={siteTitle}>
+      <Bio />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Image
+        fixed={data.file.childImageSharp.fluid}
+        style={{ width: '100%', aspectratio: 1 }}
+      />
     </Layout>
   )
 }
@@ -19,10 +25,19 @@ export default NotFoundPage
 
 export const pageQuery = graphql`
     query {
+        file(relativePath: { eq: "404.jpg" }) {
+            childImageSharp {
+                fluid(maxWidth: 500, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                    ...GatsbyImageSharpFluidLimitPresentationSize
+                }
+            }
+        }
         site {
             siteMetadata {
                 title
             }
         }
     }
+    
 `
