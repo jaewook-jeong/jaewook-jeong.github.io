0366731f3d4dd836705874488c0bbefd97c2e267
woogi <dnrlwo11@gmail.com>
2022-10-03 21:55:14 +0900

    [FEAT] h2 auto link

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-autolink-headers`,
         ],
       },
     },
