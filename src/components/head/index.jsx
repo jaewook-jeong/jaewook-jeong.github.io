332cbf94e463e4fe3d00568c1c91fa027094331a
woogi <dnrlwo11@gmail.com>
2022-10-10 20:42:24 +0900

    [FEAT] SEO위한 Head제작

---
+++ src/components/head/index.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+export function Head({ description, lang, meta, keywords, title, thumbnail }) {
+  return (
+    <StaticQuery
+      query={detailsQuery}
+      render={data => {
+        const metaDescription =
+          description || data.site.siteMetadata.description
+        return (
+          <Helmet
+            htmlAttributes={{
+              lang,
+            }}
+            title={title}
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+            meta={[
+              {
+                name: `description`,
+                content: metaDescription,
+              },
+              {
+                property: `og:title`,
+                content: title,
+              },
+              {
+                property: `og:description`,
+                content: metaDescription,
+              },
+              {
+                property: `og:type`,
+                content: `website`,
+              },
+              {
+                property: `og:image`,
+                content: thumbnail,
+              },
+              {
+                name: `twitter:card`,
+                content: `summary_large_image`,
+              },
+              {
+                name: `twitter:creator`,
+                content: data.site.siteMetadata.author,
+              },
+              {
+                name: `twitter:title`,
+                content: title,
+              },
+              {
+                name: `twitter:description`,
+                content: metaDescription,
+              },
+              {
+                name: `twitter:image`,
+                content: thumbnail,
+              },
+            ]
+              .concat(
+                keywords.length > 0
+                  ? {
+                    name: `keywords`,
+                    content: keywords.join(`, `),
+                  }
+                  : []
+              )
+              .concat(meta)}
+          />
+        )
+      }}
+    />
+  )
+}
+
+Head.defaultProps = {
+  lang: `kr`,
+  meta: [],
+  keywords: [],
+}
+
+const detailsQuery = graphql`
+    query DefaultSEOQuery {
+        site {
+            siteMetadata {
+                title
+                description
+                author
+            }
+        }
+    }
+`
