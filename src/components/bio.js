a3178353e644c26c3cfa151b6a8d889a918f8b00
jaewook-jeong <dnrlwo11@gmail.com>
2024-10-05 16:36:37 +0900

    [CHORE] invalid identifier 제거

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,31 +8,29 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import styled from "styled-components";
+import styled from "styled-components"
 
 const StyledBio = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 10px;
-  
+
   div {
     display: flex;
   }
-  
+
   .author-socials {
     gap: 10px;
     color: #1a202c;
     font-weight: 500;
   }
-  
+
   a {
-    
     :hover {
       color: #0077aa;
     }
   }
-`;
-
+`
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -71,18 +69,25 @@ const Bio = () => {
         />
         <p>
           Written by <strong>{author.name}</strong>
-          <br/>
+          <br />
           문의사항이 있다면 연락주세요:)
-          <br/>
-          <a href="https://toss.im/" target="_blank">@@toss</a> frontend
+          <br />
+          <a href="https://toss.im/" target="_blank">
+            @toss
+          </a>{" "}
+          frontend
         </p>
       </div>
       <div className="author-socials">
         {social.github && (
-          <a href={`https://github.com/${social.github}`} target="_blank">GitHub</a>
+          <a href={`https://github.com/${social.github}`} target="_blank">
+            GitHub
+          </a>
         )}
         {social.velog && (
-          <a href={`https://velog.io/${social.velog}`} target="_blank">Velog</a>
+          <a href={`https://velog.io/${social.velog}`} target="_blank">
+            Velog
+          </a>
         )}
       </div>
     </StyledBio>
